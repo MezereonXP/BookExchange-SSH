08837039a35beb3b0ee1e9683a84bacb1a7d7867
Ke <mezereonxp@gmail.com>
2017-10-09 14:43:44 +0800

    add Implementation of SERVICE

---
+++ src/service/IMessageServiceImpl.java
@@ -0,0 +1,44 @@
+package service;
+
+import dao.IMessageDaoImpl;
+import entity.MessageEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by Mezereon on 2017/10/9.
+ */
+@Service
+public class IMessageServiceImpl implements IMessageService {
+
+    @Autowired
+    private IMessageDaoImpl iMessageDao;
+
+    @Override
+    public List<MessageEntity> getAllMessage() throws SQLException {
+        return iMessageDao.getAllForum();
+    }
+
+    @Override
+    public void addMessage(MessageEntity messageEntity) throws SQLException {
+        iMessageDao.saveMessage(messageEntity);
+    }
+
+    @Override
+    public void updateMessage(MessageEntity messageEntity) throws SQLException {
+        iMessageDao.editMessage(messageEntity);
+    }
+
+    @Override
+    public void delMessage(MessageEntity messageEntity) throws SQLException {
+        iMessageDao.delMessage(messageEntity);
+    }
+
+    @Override
+    public boolean isExists(int id) throws SQLException {
+        return iMessageDao.isExists(id);
+    }
+}
